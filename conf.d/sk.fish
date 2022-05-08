bd9729ccbb033003699341bf41504c02c9a832ce
Nic Patterson <nic@reynn.dev>
2022-05-08 16:58:15 -0700

    fix casing for default_opts

--- conf.d/sk.fish
+++ conf.d/sk.fish
@@ -7,19 +7,19 @@ set --global _sk_search_vars_command '_sk_search_variables (set --show | psub) (
 # Install the default bindings, which are mnemonic and minimally conflict with fish's preset bindings
 sk_configure_bindings
 
-# If sk_DEFAULT_OPTS is not set, then set some sane defaults. This also affects sk outside of this plugin.
+# If SK_DEFAULT_OPTS is not set, then set some sane defaults. This also affects sk outside of this plugin.
 # See https://github.com/junegunn/sk#environment-variables
-if not set --query sk_DEFAULT_OPTS
+if not set --query SK_DEFAULT_OPTS
     # cycle allows jumping between the first and last results, making scrolling faster
     # layout=reverse lists results top to bottom, mimicking the familiar layouts of git log, history, and env
     # border makes clear where the sk window begins and ends
     # height=90% leaves space to see the current command and some scrollback, maintaining context of work
     # preview-window=wrap wraps long lines in the preview window, making reading easier
     # marker=* makes the multi-select marker more distinguishable from the pointer (since both default to >)
-    set --global --export sk_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+    # set --global --export SK_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
 end
 
-# Doesn't erase sk_DEFAULT_OPTS because too hard to tell if it was set by the user or by this plugin
+# Doesn't erase sK_DEFAULT_OPTS BECAUSE Too hard to tell if it was set by the user or by this plugin
 # Doesn't erase autoloaded _sk_* functions because they are not easily accessible once key bindings are erased
 function _sk_uninstall --on-event sk_uninstall
     _sk_uninstall_bindings
